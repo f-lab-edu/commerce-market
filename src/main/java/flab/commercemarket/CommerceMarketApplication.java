3c9ea7784086dfd30982a25aead1b02184f9982c
ne5s <phobos902080@gmail.com>
2023-06-19 21:41:05 +0900

    setting: 프로젝트 생성

---
+++ src/main/java/flab/commercemarket/CommerceMarketApplication.java
@@ -0,0 +1,13 @@
+package flab.commercemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceMarketApplication.class, args);
+	}
+
+}
