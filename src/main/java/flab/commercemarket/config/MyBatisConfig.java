710dd80d99fd3fc5e91c19bb8a159d346083d205
TaeHeyeon_Kim <thk98k@gmail.com>
2023-07-07 14:58:28 +0900

    refactor: mybatis 설정파일 변경
    yml파일에서 JavaConfig 파일로 설정 변경

---
+++ src/main/java/flab/commercemarket/config/MyBatisConfig.java
@@ -0,0 +1,39 @@
+package flab.commercemarket.config;
+
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("flab.commercemarket.*.mapper")
+public class MyBatisConfig {
+
+    private final DataSource dataSource;
+
+    public MyBatisConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mybatis/**/*.xml"));
+        sessionFactoryBean.setConfiguration(mybatisConfiguration());
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public org.apache.ibatis.session.Configuration mybatisConfiguration() {
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        return configuration;
+    }
+}
+
