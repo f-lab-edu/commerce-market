744d68a5371526bd8cb98be1cffe7045eddc5bf2
TaeHeyeon_Kim <thk98k@gmail.com>
2023-08-20 12:19:59 +0900

    feat: Access Token 발급 로직

---
+++ src/main/java/flab/commercemarket/config/JasyptConfig.java
@@ -0,0 +1,34 @@
+package flab.commercemarket.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(encryptKey);
+
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+
+    }
+}
--- src/main/java/flab/commercemarket/domain/payment/PaymentService.java
+++ src/main/java/flab/commercemarket/domain/payment/PaymentService.java
@@ -1,12 +1,20 @@
 package flab.commercemarket.domain.payment;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import flab.commercemarket.common.exception.DataNotFoundException;
 import flab.commercemarket.common.exception.DuplicateDataException;
 import flab.commercemarket.domain.payment.mapper.PaymentMapper;
 import flab.commercemarket.domain.payment.vo.Payment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +23,41 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
+
+    @Value("${imp_key}")
+    private String apiKey;
+
+    @Value("${imp_secret}")
+    private String apiSecret;
+
     private final PaymentMapper paymentMapper;
+
+    public String getToken() {
+        String apiUrl = "https://api.iamport.kr/users/getToken";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String requestBody = String.format("{\"imp_key\": \"%s\", \"imp_secret\": \"%s\"}", apiKey, apiSecret);
+        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(apiUrl, requestEntity, String.class);
+
+        String responseBody = responseEntity.getBody();
+        log.info("responseEntity.getBody() = {}", responseBody);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode responseJson = objectMapper.readTree(responseBody);
+            JsonNode accessTokenNode = responseJson.get("response").get("access_token");
+            return accessTokenNode.asText();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
     public Payment processPayment(Payment payment) {
         // TODO 결제 요청을 받아 가맹점 DB에 저장하고 PG사에 결제 요청을 보내는 로직
